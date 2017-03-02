82f8211a000e51a076d04d8ab9c61c3579bb32ab
Francois Fouquet <fouquet.f@gmail.com>
2017-03-02 19:17:26 +0100

    update blog trunk

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+export LC_TIME=en_US.UTF-8
+if [ -d "public" ]; then
+  cd public
+  git pull
+  cd ..
+else
+  git clone git@github.com:datathings/datathings.github.io.git public
+fi
+hugo
+cd public/
+git add -A
+MESSAGE="Site rebuild $(date)"
+git commit -m "$MESSAGE"
+git push
